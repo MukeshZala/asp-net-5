25dfcb4817404bf06c7fa127a497dafe5f487a24
Mukesh Zala <Mukesh.Zala@outlook.com>
2021-09-14 15:02:25 -0500

    Category Table

--- asp-net-ef/WizLib/WizLib_DataAccess/Data/ApplicationDbContext.cs
+++ asp-net-ef/WizLib/WizLib_DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,8 @@ namespace WizLib_DataAccess.Data
         //public DbSet<Category> Categories { get; set; }
         public DbSet<Genre>  Genres { get; set; }
 
+         
+        public DbSet<Category> Category { get; set; }
         public DbSet<Book> Books { get; set; }
 
         public DbSet<Author> Authors { get; set; }
---
+++ asp-net-ef/WizLib/WizLib_Model/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizLib_Model.Models
+{
+    [Table("tbl_Category")]
+   public class Category
+    {
+        [Key]
+        public int Category_Id { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+    }
+}
